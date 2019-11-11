7a555c63846b3a77f0d5754217b01677ad0edee6
yangxia <yangxia@bytedance.com>
2019-11-11 15:10:33 +0800

    chore: 改构建

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
-set -x
-npm run build
-set +x
+# echo 'The following "npm" command builds your Node.js/React application for'
+# echo 'production in the local "build" directory (i.e. within the'
+# echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
+# echo 'correctly bundles React in production mode and optimizes the build for'
+# echo 'the best performance.'
+# set -x
+# npm run build
+# set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
@@ -18,7 +18,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+npm serve &
 sleep 1
 echo $! > .pidfile
 set +x

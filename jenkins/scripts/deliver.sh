00541f7c5ecec69726a0fa1b941e4dd37ee9e156
yangxia <yangxia@bytedance.com>
2019-11-11 15:41:36 +0800

    chore: 脚本需要有run

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 set -x
-npm serve &
+npm run serve &
 sleep 1
 echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:8081 to see your Node.js/React application in action.'
+echo 'Visit http://localhost:8081 to see your Node.js application in action.'
 echo '(This is why you specified the "args ''-p 8081:8081''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
